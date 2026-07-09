00c77804413ce9a6d339fa194e2f800567e3ac68
ThinhB2306642 <thinhb2306642@student.ctu.edu.vn>
2026-07-10 00:21:52 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const contactsRouter = require("./app/routes/contact.route");
 const app = express();
+
+
 app.use(cors());
 app.use(express.json());
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to contact book application." });
-});
+app.use("/api/contacts", contactsRouter);
 module.exports = app;
